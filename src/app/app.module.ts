3ea5561d1dfcb53d1354a37f39c59ccb3aec87f5
hulikeri <lingaraj_hulikeri@epam.com>
2022-06-13 22:15:29 +0530

    ComponentWithinaComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MediaItmesComponent } from './media-itmes/media-itmes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaItmesComponent
   ],
   imports: [
     BrowserModule,
