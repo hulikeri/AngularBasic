ef1873f6647299e09da6ad885626c07fe50d0619
hulikeri <lingaraj_hulikeri@epam.com>
2022-06-13 18:17:30 +0530

    Added styles to appcomponent.scss class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [`
-  h1{color: blue;}
-  h2{color: yellow;}
-  h3{color: green;}
-  h4{color: maroon;}
-  .BodyProperties{background-color: Orange;}
-  `]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Software'
